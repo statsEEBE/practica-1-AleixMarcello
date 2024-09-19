63b7379c2a84b6619e35c5c849b9eb9d80b73019
Your Name <you@example.com>
2024-09-19 11:26:17 +0200

    a

--- Problema1.R
+++ Problema1.R
@@ -9,18 +9,23 @@ qsec_intervalos = cut(mis_dades$qsec, breaks = 4)
 table(qsec_intervalos)
 
 hist(mis_dades$qsec, breaks = 10)
+
+#Pregunta1
 #mitjana -> mean()
 mean(mis_dades$qsec)
 
+#Pregunta2
 #mediana -> median()
 mis_dades$drat
 median(mis_dades$drat)
 
+#Pregunta3
 #primer quartil -> quantile(x, 0.25)
 quantile(mis_dades$drat)
 
+#Pregunta4
 quantile(mis_dades$mpg, 0.18)
-
+----------------------------------------
 #taula de freqüencia
 
 sum(table(qsec_intervalos))
@@ -35,7 +40,11 @@ pie(fi)
 Ni = cumsum(ni)
 Fi = cumsum(fi)
 cbind(ni, fi, Ni, Fi)
+-----------------------------------------
 
+#Pregunta5
 #rango interquartilico -> IQR
 IQR(mis_dades$cyl)
 boxplot(mis_dades$cyl)
+
+#Pregunta6
