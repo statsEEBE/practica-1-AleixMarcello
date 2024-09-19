f063b0c7a3d2112c3e31ef0a3399abefcd4e80b2
Your Name <you@example.com>
2024-09-19 11:18:22 +0200

    a

--- Problema1.R
+++ Problema1.R
@@ -4,3 +4,34 @@ plot(1:5)
 mis_dades = mtcars
 mis_dades$qsec
 sort(mis_dades$qsec)
+
+qsec_intervalos = cut(mis_dades$qsec, breaks = 4)
+table(qsec_intervalos)
+
+hist(mis_dades$qsec, breaks = 10)
+#mitjana -> mean()
+mean(mis_dades$qsec)
+
+#mediana -> median()
+mis_dades$drat
+median(mis_dades$drat)
+
+#primer quartil -> quantile(x, 0.25)
+quantile(mis_dades$drat)
+
+quantile(mis_dades$mpg, 0.18)
+
+#taula de freqüencia
+
+sum(table(qsec_intervalos))
+ni = (table(qsec_intervalos))
+sum(ni)
+
+fi= ni/sum(ni)
+fi
+pie(fi)
+
+
+Ni = cumsum(ni)
+Fi = cumsum(fi)
+cbind(ni, fi, Ni, Fi)
